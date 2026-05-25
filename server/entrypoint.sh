d625e14e4ff489b10c21ed605ce77720f1e58611
hanisx019 <anisx019@gmail.com>
2026-05-25 12:41:13 +0530

    Make entrypoint migrations verbose and fail if errors

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Run database migrations on container start (non-interactive)
 echo "Running migrations..."
-python manage.py migrate --noinput || true
+python manage.py migrate --noinput --verbosity 2
 echo "Migrations finished, starting server..."
 
 exec gunicorn core.wsgi --bind 0.0.0.0:8080
