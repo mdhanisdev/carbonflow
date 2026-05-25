e93dcfa68f78c7c6c940bb1046a5ad1811115a7d
hanisx019 <anisx019@gmail.com>
2026-05-25 11:36:36 +0530

    Auto-run migrations at startup to ensure production DB tables

--- server/core/wsgi.py
+++ server/core/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+import logging
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+# Run migrations on startup (helps ensure DB tables exist in ephemeral deploys)
+try:
+	call_command('migrate', interactive=False)
+except Exception as e:
+	logging.exception('Auto-migrate failed on startup: %s', e)
